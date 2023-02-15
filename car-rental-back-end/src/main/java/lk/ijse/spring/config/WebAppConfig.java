8329cd684517da874c92f03ac78c0634a9f9661b
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-15 19:34:28 +0530

    Update content

--- car-rental-back-end/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,9 +1,19 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @author : Gihan Madhusankha
  * 2023-02-15 6:21 PM
  **/
 
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring")
+@EnableWebMvc
 public class WebAppConfig {
+
+
+
 }
--- car-rental-back-end/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -1,9 +1,12 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author : Gihan Madhusankha
  * 2023-02-15 6:21 PM
  **/
 
+@Configuration
 public class WebRootConfig {
 }
