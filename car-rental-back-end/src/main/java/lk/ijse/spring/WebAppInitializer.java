ad504e42eabc29b497f90981ac8dbd307afea523
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-15 18:22:34 +0530

    Update root

---
+++ car-rental-back-end/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring;
+
+/**
+ * @author : Gihan Madhusankha
+ * 2023-02-15 6:20 PM
+ **/
+
+public class WebAppInitializer {
+}
---
+++ car-rental-back-end/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.config;
+
+/**
+ * @author : Gihan Madhusankha
+ * 2023-02-15 6:21 PM
+ **/
+
+public class WebAppConfig {
+}
