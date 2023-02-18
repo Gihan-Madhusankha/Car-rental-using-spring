faf24f8fc02c5d564ccb5a9684e3938f080c4ee1
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-18 09:37:07 +0530

    Update content

--- car-rental-front-end/assets/js/customer.js
+++ car-rental-front-end/assets/js/customer.js
@@ -3,6 +3,26 @@ $('#btnCustomerLogin').click(function (){
     $('#main').css('display', 'none');
     $('#footer').css('display', 'none');
     $('.cusSignUp').css('display', 'none');
+    $('.driverLogin').css('display', 'none');
+    $('.adminLogin').css('display', 'none');
+});
+
+$('#btnDriverLogin').click(function (){
+    $('.driverLogin').css('display', 'block');
+    $('#main').css('display', 'none');
+    $('#footer').css('display', 'none');
+    $('.cusSignUp').css('display', 'none');
+    $('.adminLogin').css('display', 'none');
+    $('.cusLogin').css('display', 'none');
+});
+
+$('#btnAdminLogin').click(function (){
+    $('.adminLogin').css('display', 'block');
+    $('#main').css('display', 'none');
+    $('#footer').css('display', 'none');
+    $('.cusSignUp').css('display', 'none');
+    $('.cusLogin').css('display', 'none');
+    $('.driverLogin').css('display', 'none');
 });
 
 
@@ -12,11 +32,15 @@ $('#customerSignUp').click(function () {
     $('#main').css('display', 'none');
     $('#footer').css('display', 'none');
     $('.cusLogin').css('display', 'none');
+    $('.driverLogin').css('display', 'none');
+    $('.adminLogin').css('display', 'none');
 });
 
-$('#cusSignUpCancel, #btnBack').click(function(){
+$('#cusSignUpCancel, .btnBack').click(function(){
     $('.cusSignUp').css('display', 'none');
     $('.cusLogin').css('display', 'none');
+    $('.adminLogin').css('display', 'none');
+    $('.driverLogin').css('display', 'none');
     $('#main').css('display', 'block');
     $('#footer').css('display', 'flex');
 });
