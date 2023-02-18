178d64bb7db095b8fa01f06806b4f9699f8a933d
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-18 10:25:43 +0530

    Update content

--- car-rental-front-end/assets/js/customer.js
+++ car-rental-front-end/assets/js/customer.js
@@ -1,30 +1,32 @@
-$('#btnCustomerLogin').click(function (){
+$('#btnCustomerLogin').click(function () {
     $('.cusLogin').css('display', 'block');
     $('#main').css('display', 'none');
     $('#footer').css('display', 'none');
     $('.cusSignUp').css('display', 'none');
     $('.driverLogin').css('display', 'none');
     $('.adminLogin').css('display', 'none');
+    $('#adminDash').css('display', 'none');
 });
 
-$('#btnDriverLogin').click(function (){
+$('#btnDriverLogin').click(function () {
     $('.driverLogin').css('display', 'block');
     $('#main').css('display', 'none');
     $('#footer').css('display', 'none');
     $('.cusSignUp').css('display', 'none');
     $('.adminLogin').css('display', 'none');
     $('.cusLogin').css('display', 'none');
+    $('#adminDash').css('display', 'none');
 });
 
-$('#btnAdminLogin').click(function (){
+$('#btnAdminLogin').click(function () {
     $('.adminLogin').css('display', 'block');
     $('#main').css('display', 'none');
     $('#footer').css('display', 'none');
     $('.cusSignUp').css('display', 'none');
     $('.cusLogin').css('display', 'none');
     $('.driverLogin').css('display', 'none');
+    $('#adminDash').css('display', 'none');
 });
-
 
 
 $('#customerSignUp').click(function () {
@@ -34,13 +36,25 @@ $('#customerSignUp').click(function () {
     $('.cusLogin').css('display', 'none');
     $('.driverLogin').css('display', 'none');
     $('.adminLogin').css('display', 'none');
+    $('#adminDash').css('display', 'none');
 });
 
-$('#cusSignUpCancel, .btnBack').click(function(){
+$('#cusSignUpCancel, .btnBack').click(function () {
     $('.cusSignUp').css('display', 'none');
     $('.cusLogin').css('display', 'none');
     $('.adminLogin').css('display', 'none');
     $('.driverLogin').css('display', 'none');
     $('#main').css('display', 'block');
+    $('#footer').css('display', 'flex');
+    $('#adminDash').css('display', 'none');
+});
+
+$('#adminLoginBtn').click(function () {
+    $('#adminDash').css('display', 'block');
+    $('.cusSignUp').css('display', 'none');
+    $('.cusLogin').css('display', 'none');
+    $('.adminLogin').css('display', 'none');
+    $('.driverLogin').css('display', 'none');
+    $('#main').css('display', 'none');
     $('#footer').css('display', 'flex');
 });
