16012fbb74e0503d9834fe916d0aceafc9721824
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-21 23:57:53 +0530

    Added controllers for controller directory

---
+++ car-rental-back-end/src/main/java/lk/ijse/spring/controller/DriverController.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.controller;
+
+/**
+ * @author : Gihan Madhusankha
+ * 2023-02-21 11:56 PM
+ **/
+
+public class DriverController {
+}
---
+++ car-rental-back-end/src/main/java/lk/ijse/spring/controller/UserController.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.controller;
+
+/**
+ * @author : Gihan Madhusankha
+ * 2023-02-21 11:57 PM
+ **/
+
+public class UserController {
+}
