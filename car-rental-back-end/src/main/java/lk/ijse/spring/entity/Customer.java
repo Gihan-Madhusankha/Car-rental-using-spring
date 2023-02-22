1890e30ebda4fdd34dca2c6d0402f3a17d4974b6
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-22 13:27:01 +0530

    Created the customer entity

--- car-rental-back-end/src/main/java/lk/ijse/spring/entity/Customer.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,9 +1,34 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * @author : Gihan Madhusankha
  * 2023-02-21 11:52 PM
  **/
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Customer {
+    private String fullName;
+    @Id
+    private String userName;
+    private String password;
+    private String address;
+    private int contact;
+    private String email;
+    private int nicNo;
+    private int licenseNo;
+    private String customerImage;
+    private String nicImage;
+    private String licenseImage;
 }
