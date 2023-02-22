bfc7ad1b3eb656994248482515dfb10430e5a92d
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-22 14:20:06 +0530

    Complete the save customer method in customer service impl

--- car-rental-back-end/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.service;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,12 @@ public class CustomerServiceImpl {
     @Autowired
     ModelMapper modelMapper;
 
-
+    public void saveCustomer(CustomerDTO dto){
+        if (repo.existsById(dto.getUserName())) {
+            throw new RuntimeException(dto.getUserName()+" is Already Exists");
+        }
+        Customer entity = modelMapper.map(dto, Customer.class);
+        repo.save(entity);
+    }
 
 }
