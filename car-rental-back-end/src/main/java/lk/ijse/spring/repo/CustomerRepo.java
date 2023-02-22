e09b8254de9fb1b665f1998b96e1eab80d1cd5b0
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-22 14:15:54 +0530

    Refactored the customerRepo

--- car-rental-back-end/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
@@ -1,10 +1,12 @@
 package lk.ijse.spring.repo;
 
+import lk.ijse.spring.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @author : Gihan Madhusankha
  * 2023-02-21 11:54 PM
  **/
 
-public interface CustomerRepo {
-
+public interface CustomerRepo extends JpaRepository<Customer, String> {
 }
--- car-rental-back-end/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.repo.CustomerRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
