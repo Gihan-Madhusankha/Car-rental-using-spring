e9c675c38a90bc69cc7bf9b7e8f377cd869ef96f
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-26 13:08:40 +0530

    Create admin entity

--- car-rental-back-end/src/main/java/lk/ijse/spring/entity/Admin.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -1,9 +1,32 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * @author : Gihan Madhusankha
  * 2023-02-21 11:51 PM
  **/
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Admin {
+    @Id
+    @Column(nullable = false)
+    private String adminID;
+    @Column(nullable = false)
+    private String userName;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private int contact;
 }
