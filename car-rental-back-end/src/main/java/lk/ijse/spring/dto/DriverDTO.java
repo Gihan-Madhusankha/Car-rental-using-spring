9d270565a62ba615cd58062df21220d4c64fd364
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-26 19:17:00 +0530

    Added driver dto

---
+++ car-rental-back-end/src/main/java/lk/ijse/spring/dto/DriverDTO.java
@@ -0,0 +1,26 @@
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+
+/**
+ * @author : Gihan Madhusankha
+ * 2023-02-26 7:15 PM
+ **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class DriverDTO {
+    private String driverID;
+    private String driverName;
+    private String driverAddress;
+    private int age;
+    private int contact;
+    private String releaseOrNot;
+}
