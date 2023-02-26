1e2342a0d450003c60d02fa19f40c71457d475c6
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-26 14:02:15 +0530

    Update content

--- car-rental-back-end/src/main/java/lk/ijse/spring/controller/AdminController.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/controller/AdminController.java
@@ -1,9 +1,15 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.AdminDTO;
+import lk.ijse.spring.service.AdminService;
 import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 /**
  * @author : Gihan Madhusankha
@@ -15,10 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class AdminController {
 
+    @Autowired
+    AdminService service;
 
-
+    @GetMapping
     public ResponseUtil getAllAdmins(){
-
+        ArrayList<AdminDTO> allAdmins = service.allAdmins();
+        return new ResponseUtil("200","Success", allAdmins);
     }
 
 }
--- car-rental-back-end/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
@@ -1,11 +1,17 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.AdminDTO;
+import lk.ijse.spring.entity.Admin;
+import lk.ijse.spring.repo.AdminRepo;
 import lk.ijse.spring.service.AdminService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : Gihan Madhusankha
@@ -16,10 +22,15 @@ import java.util.ArrayList;
 @Transactional
 public class AdminServiceImpl implements AdminService {
 
-
+    @Autowired
+    AdminRepo repo;
+    
+    @Autowired
+    ModelMapper modelMapper;
 
     @Override
     public ArrayList<AdminDTO> allAdmins() {
-        return null;
+        return modelMapper.map(repo.findAll(), new TypeToken<ArrayList<AdminDTO>>(){
+        }.getType());
     }
 }
