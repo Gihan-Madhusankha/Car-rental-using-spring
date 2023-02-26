63490c42c5729f2d2b126b431bc2e53cfffcc96b
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2023-02-26 19:46:38 +0530

    Update content

--- car-rental-back-end/src/main/java/lk/ijse/spring/controller/DriverController.java
+++ car-rental-back-end/src/main/java/lk/ijse/spring/controller/DriverController.java
@@ -1,9 +1,19 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : Gihan Madhusankha
  * 2023-02-21 11:56 PM
  **/
 
+@RestController
+@RequestMapping("/driver")
+@CrossOrigin
 public class DriverController {
+
+
+
 }
--- car-rental-front-end/assets/js/driver.js
+++ car-rental-front-end/assets/js/driver.js
@@ -14,7 +14,7 @@ driverLoginValidations.push({
 driverLoginValidations.push({
     reg: passwordDrRegEx,
     field: $('#passwordDr'),
-    error: 'password pattern is wrong.! ex:0-9 5 digits'
+    error: 'password pattern is wrong.! ex:A-z 0-9 5 digits'
 });
 
 function checkValidityDr() {
@@ -51,3 +51,49 @@ function setErrorDr(textField, msg) {
         textField.parent().children('span').text(msg);
     }
 }
+
+//=============================================================
+
+$('#adUserName, #adPassword1, #driverId, #driverName, #driverAddress, #driverAge, #driverContact, #driverReleaseOrNot').on('keyup', function () {
+    checkValidityDr();
+});
+
+const usernameAdRegEx = /^[A-z]{3,10}$/;
+const passwordAdRegEx = /^[A-z0-9]{5}$/;
+const driverNameRegEx = /^[A-z .]{5,}$/;
+const driverAddressRegEx = /^[0-9/A-z. ,]{5,}$/;
+const driverAgeRegEx = /^[1-9][0-9]$/;
+const driverContactNumRegEx = /^(07)[01245678][0-9]{7}$/;
+
+driverLoginValidations.push({
+    reg: usernameAdRegEx,
+    field: $('#adUserName'),
+    error: 'username pattern is wrong.! ex: A-z 0-9'
+});
+driverLoginValidations.push({
+    reg: passwordAdRegEx,
+    field: $('#adPassword1'),
+    error: 'password pattern is wrong.! ex: A-z 0-9 5 digits'
+});
+driverLoginValidations.push({
+    reg: driverNameRegEx,
+    field: $('#driverName'),
+    error: 'driver name pattern is wrong.! ex: A-z'
+});
+driverLoginValidations.push({
+    reg: driverAddressRegEx,
+    field: $('#driverAddress'),
+    error: 'driver address pattern is wrong.! ex: A-z 0-9, ./'
+});
+driverLoginValidations.push({
+    reg: driverAgeRegEx,
+    field: $('#driverAge'),
+    error: 'driver age pattern is wrong.! ex: 0-9'
+});
+driverLoginValidations.push({
+    reg: driverContactNumRegEx,
+    field: $('#driverContact'),
+    error: 'driver contact pattern is wrong.! ex: 0-9 10 digits'
+});
+
+
